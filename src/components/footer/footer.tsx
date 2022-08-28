48b8cb31edff56d57640421538f77b97c84d14b6
indexofmayank <maimayanmanu@gmail.com>
2022-08-28 20:46:24 +0530

    first

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -16,10 +16,13 @@ const Footer: FC = () => {
           <Grid item xs={12} md={5}>
             <Box sx={{ width: { xs: '100%', md: 360 }, mb: { xs: 3, md: 0 } }}>
               <Typography component="h2" variant="h2" sx={{ mb: 2 }}>
-                Coursespace
+                EngineerUncle - Gigaai
               </Typography>
               <Typography variant="subtitle1" sx={{ letterSpacing: 1, mb: 2 }}>
-                Coursespace is an online learning platform that has been operating since 2018 until now.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi quod facilis alias dolorum delectus? 
+              </Typography>
+              <Typography variant="subtitle1" sx={{ letterSpacing: 1, mb: 2 }}>
+                Webel Bhawan - Webel Bhavan Block: EP & GP, Sector V, Bidhannagar, Salt Lake, Kolkata: 700 091 
               </Typography>
               <FooterSocialLinks />
             </Box>
